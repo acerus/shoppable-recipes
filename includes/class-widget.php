3ec879e16519edfca70b634e718984bb808f3d42
pavel <paul.fedorov@gmail.com>
2020-12-23 01:24:05 +0300

    show widget on frontend

--- includes/class-widget.php
+++ includes/class-widget.php
@@ -15,37 +15,32 @@ class Widget {
 	 *
 	 * @param WP_OSA $wposa_obj
 	 */
-	public function __construct( $wposa_obj ) {
+	public function __construct($wposa_obj) {
 		$this->wposa_obj = $wposa_obj;
 
 		$this->setup();
 		$this->hooks();
 	}
 
-	public function setup() {}
+	public function setup() {
+	}
 
 	public function hooks() {
-		add_action( 'init', [ $this, 'enqueue_scripts' ] );
-		add_action( 'wp_footer', [ $this, 'init_widget' ] );
-		add_filter( 'the_content', [ $this, 'append_widget_to_content' ] );
-		add_action( 'woocommerce_after_single_product_summary', [ $this, 'append_widget_to_woocommerce_product' ], 30 );
-		add_shortcode( 'mihdan-mailru-pulse-widget', [ $this, 'shortcode' ] );
+		add_action('wp_enqueue_scripts', [$this, 'enqueue_scripts']);
+		add_filter('the_content', [$this, 'append_widget_to_content']);
+		add_action('woocommerce_after_single_product_summary', [$this, 'append_widget_to_woocommerce_product'], 30);
+		add_shortcode('mihdan-mailru-pulse-widget', [$this, 'shortcode']);
 	}
 
-	public function enqueue_scripts(){
-		wp_enqueue_script( "whisk-sl", "https://cdn.whisk.com/sdk/shopping-list.js", [], '', false );
+	public function enqueue_scripts() {
+		wp_enqueue_script("whisk-sl", "https://cdn.whisk.com/sdk/shopping-list.js", [], '', false);
+		wp_add_inline_script( 'whisk-sl', 'var whisk = whisk || {};
+  whisk.queue = whisk.queue || [];
+
+  whisk.queue.push(function () {
+    whisk.shoppingList.defineWidget("MBAO-GAEI-PTUR-ORAJ");
+  });' );
 	}
-
-	public function init_widget(){ ?>
-		<script>
-		var whisk = whisk || {};
-		whisk.queue = whisk.queue || [];
-
-		whisk.queue.push(function () {
-			whisk.shoppingList.defineWidget("NVFU-JQJX-MXIR-EIQJ");
-		});
-		</script>
-	<?php }
 
 	/**
 	 * Add shortcode `[mihdan-mailru-pulse-widget]`.
@@ -63,8 +58,8 @@ class Widget {
 	 *
 	 * @return string
 	 */
-	public function append_widget_to_content( $content ) {
-		if ( is_singular( $this->wposa_obj->get_option( 'post_types', 'feed' ) ) && 'on' === $this->wposa_obj->get_option( 'auto_append', 'widget' ) ) {
+	public function append_widget_to_content($content) {
+		if (is_singular() && 'on' === $this->wposa_obj->get_option('auto_append', 'shopping-list')) {
 			$content .= $this->html();
 		}
 
@@ -75,7 +70,7 @@ class Widget {
 	 * Append Pulse widget to WooCommerce product single page.
 	 */
 	public function append_widget_to_woocommerce_product() {
-		if ( is_singular( 'product' ) && 'on' === $this->wposa_obj->get_option( 'auto_append', 'widget' ) ) {
+		if (is_singular('product') && 'on' === $this->wposa_obj->get_option('auto_append', 'widget')) {
 			echo $this->html();
 		}
 	}
@@ -86,10 +81,12 @@ class Widget {
 	 * @return string
 	 */
 	public function html() {
-		return sprintf(
-			'<div class="pulse-widget" data-sid="%s"></div><script async src="https://static.pulse.mail.ru/pulse-widget.js"></script>',
-			esc_attr( $this->wposa_obj->get_option( 'id', 'widget' ) )
-		);
+
+		$html = '<div id="MBAO-GAEI-PTUR-ORAJ">';
+		$html .= '<script>whisk.queue.push(function () { whisk.display("MBAO-GAEI-PTUR-ORAJ"); }); </script>';
+		$html .= '</div>';
+
+		return $html;
 	}
 }
 
